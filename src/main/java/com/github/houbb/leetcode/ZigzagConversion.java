e8e2bb0eac0946811b19f299244496304693d58c
houbb <1060732496@qq.com>
2020-06-09 23:54:04 +0800

    [Feature] add for new

---
+++ src/main/java/com/github/houbb/leetcode/ZigzagConversion.java
@@ -0,0 +1,19 @@
+package com.github.houbb.leetcode;
+
+/**
+ * 以 Z 字形从上到下排列字符，返回的时候从左到右。
+ *
+ * <p> project: leetcode-ZigzagConversion </p>
+ * <p> create on 2020/6/9 23:52 </p>
+ *
+ * @author binbin.hou
+ * @since v1
+ */
+public class ZigzagConversion {
+
+    public String convert(String s, int numRows) {
+
+        return "";
+    }
+
+}
