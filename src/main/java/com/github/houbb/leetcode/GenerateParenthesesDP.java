862f8bd3b54525c634cf25e9e5d33a41ee834641
binbin.hou <binbin.hou@huifu.com>
2020-06-17 17:58:28 +0800

    [Feature] add for new

---
+++ src/main/java/com/github/houbb/leetcode/GenerateParenthesesDP.java
@@ -0,0 +1,36 @@
+package com.github.houbb.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 动态规划
+ *
+ * 符合的序列：(a)b
+ *
+ *
+ *
+ * @author binbin.hou
+ * @since 1.0.0
+ * @date 2020-6-17 17:51:48
+ */
+public class GenerateParenthesesDP {
+
+    /**
+     *
+     * 思路：
+     *
+     * 1. 所以都符合这种格式：(a)b
+     * 2. 生成一个 a，在生成对应的 b;  拼接就是结果。
+     *
+     *
+     * @param n 数字
+     * @return 结果
+     * @since v1
+     */
+    public List<String> generateParenthesis(int n) {
+        List<String> resultList = new ArrayList<>();
+        return resultList;
+    }
+
+}
