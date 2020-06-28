2a1213a67d28641c0391d0f3652552a626e65eed
binbin.hou <binbin.hou@huifu.com>
2020-06-28 17:42:01 +0800

    [Feature] add for new

---
+++ src/main/java/com/github/houbb/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,34 @@
+package com.github.houbb.leetcode;
+
+/**
+ * @author binbin.hou
+ * @since 1.0.0
+ */
+public class RemoveDuplicatesFromSortedArray {
+
+    /**
+     * 思路
+     *
+     * 1. 返回的结果要对
+     * 2. 返回的长度对应的数组要唯一。
+     *
+     * 【效果】
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Duplicates from Sorted Array.
+     * Memory Usage: 41.3 MB, less than 59.78% of Java online submissions for Remove Duplicates from Sorted Array.
+     *
+     * @param nums 数组
+     * @return 结果
+     * @since v1
+     */
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int n : nums) {
+            // 将后面不同的元素复制过来
+            if (i == 0 || n > nums[i-1]) {
+                nums[i++] = n;
+            }
+        }
+        return i;
+    }
+
+}
