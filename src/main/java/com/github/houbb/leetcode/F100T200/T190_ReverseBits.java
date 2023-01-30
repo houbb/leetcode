8e15c49256357775acf6d302e4da8c7c13a3da62
binbin.hou <binbin.hou@huifu.com>
2023-01-30 18:12:56 +0800

    add 190

---
+++ src/main/java/com/github/houbb/leetcode/F100T200/T190_ReverseBits.java
@@ -0,0 +1,10 @@
+package com.github.houbb.leetcode.F100T200;
+
+public class T190_ReverseBits {
+
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+
+}
