2df2c8fd6ab21e04143b981779d08a6ba600cafc
binbin.hou <binbin.hou@huifu.com>
2025-07-24 00:36:26 +0800

    add for BinarySearch_V1_T704

---
+++ src/main/java/com/github/houbb/leetcode/topics/binarySearch/BinarySearch_V1_T704.java
@@ -0,0 +1,32 @@
+package com.github.houbb.leetcode.topics.binarySearch;
+
+public class BinarySearch_V1_T704 {
+
+    public int search(int[] nums, int target) {
+        // 二分查找
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left <= right) {
+            // 中点
+            int mid = left + (right - left) / 2;
+            int midVal = nums[mid];
+            if(target == midVal) {
+                return mid;
+            }
+
+            // 小于，那么目标值应该在右边
+            if(midVal < target) {
+                left = mid+1;
+            }
+            // 大于，则目标值应该在左边
+            if(midVal > target) {
+                right = mid-1;
+            }
+        }
+
+        // not found
+        return -1;
+    }
+
+}
